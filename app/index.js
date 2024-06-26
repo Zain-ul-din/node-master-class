8cdc221eb390afd9fc46d9e41532ac73ce591840
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-06-26 18:46:10 +0500

    extract request url

--- app/index.js
+++ app/index.js
@@ -1,10 +1,46 @@
 const http = require("http");
+const url = require("url");
+const path = require("path");
 
 const serverConfig = {
   port: 3000,
 };
 
+/**
+ * trim url path also removes double slashes from url
+ *
+ * Example:
+ * ```md
+ * /foo//bar// => /foo/bar
+ * /hello/ => /hello
+ * / => /
+ * ```
+ * @param {string} url
+ * @returns string
+ */
+const trimUrl = (url) => {
+  return (
+    "/" + // since first segment always gonna be empty string
+    url
+      .split("/")
+      .filter((segment) => segment.length > 0)
+      .join("/")
+  );
+};
+
 const server = http.createServer((req, res) => {
+  /**
+   * Extract Request path from the request object
+   */
+  const requestedUrl = url.parse(req.url);
+  const { pathname } = requestedUrl;
+  const trimmedPath = trimUrl(pathname);
+
+  console.log("raw url: ", req.url);
+  console.log("path name: ", pathname);
+  console.log("trimmed path: ", trimmedPath);
+
+  // end request
   res.write("hello world");
   res.end();
 });
